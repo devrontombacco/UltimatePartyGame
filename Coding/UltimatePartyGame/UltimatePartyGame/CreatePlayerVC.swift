8c3c4b3adf31ad204e9029b535dc86454ec534e7
Dtombacco <tombacco.devron@gmail.com>
2020-05-14 15:50:08 +0100

    Add MVC folder structure + all ViewControllers

---
+++ Coding/UltimatePartyGame/UltimatePartyGame/CreatePlayerVC.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePlayerVC.swift
+//  UltimatePartyGame
+//
+//  Created by Devron Tombacco on 14/05/2020.
+//  Copyright © 2020 dtomswift. All rights reserved.
+//
+
+import UIKit
+
+class CreatePlayerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Coding/UltimatePartyGame/UltimatePartyGame/GameVC.swift
@@ -0,0 +1,30 @@
+//
+//  GameVC.swift
+//  UltimatePartyGame
+//
+//  Created by Devron Tombacco on 14/05/2020.
+//  Copyright © 2020 dtomswift. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
