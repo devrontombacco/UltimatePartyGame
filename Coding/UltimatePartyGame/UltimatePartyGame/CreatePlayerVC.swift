2e8ed08a3fab21c1bd05ba6bac7fbcf6e5e43dd4
Dtombacco <tombacco.devron@gmail.com>
2020-05-18 12:11:28 +0100

    Add UI to CreatePlayerVC

--- Coding/UltimatePartyGame/UltimatePartyGame/CreatePlayerVC.swift
+++ Coding/UltimatePartyGame/UltimatePartyGame/CreatePlayerVC.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class CreatePlayerVC: UIViewController {
 
+    @IBOutlet weak var addPlayerLabel: UILabel!
+    
+    @IBOutlet weak var addPlayerButtonOutlet: UIButton!
+    
+    @IBOutlet weak var readyButtonOutlet: UIButton!
+    
+    @IBOutlet weak var infoButtonOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addPlayerButtonOutlet.layer.borderColor = UIColor(red:166/255, green:9/255, blue:235/255, alpha: 1).cgColor
+        addPlayerButtonOutlet.layer.borderWidth = 1.0
+        addPlayerButtonOutlet.layer.cornerRadius = 10
+        
+        readyButtonOutlet.layer.borderColor = UIColor(red:0/255, green:255/255, blue:118/255, alpha: 1).cgColor
+        
+        readyButtonOutlet.layer.cornerRadius = 10
+        readyButtonOutlet.layer.borderWidth = 1.0
     }
     
-
+    @IBAction func infoButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func addPlayerButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func readyButtonAction(_ sender: UIButton) {
+    }
+    
 }
